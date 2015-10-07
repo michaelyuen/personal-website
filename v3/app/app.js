ea22cd3e7aec5dffc3d4e498449930da792751f3
Michael Yuen <michaelclaytonyuen@gmail.com>
2015-10-07 17:18:46 -0400

    added catch for nonexistent routes

--- v3/app/app.js
+++ v3/app/app.js
@@ -14,8 +14,8 @@ angular.module('personal_website', [
     $routeProvider.when('/:page_name?', { controller: 'MainController' });
 }])
 
-.controller('MainController', ['$rootScope', '$scope', '$animate', '$timeout', '$route',
-    function($rootScope, $scope, $animate, $timeout, $route) {
+.controller('MainController', ['$rootScope', '$scope', '$location', '$animate', '$timeout', '$route',
+    function($rootScope, $scope, $location, $animate, $timeout, $route) {
     
         $scope.menu_open = false;
 
@@ -27,14 +27,21 @@ angular.module('personal_website', [
             $scope.menu_open = false;
         }
 
-        $scope.$on('$routeChangeStart',function(event, next, current){
+        $scope.$on('$routeChangeStart', function (event, next, current) {
 
             if (next.params.page_name) {
-                $rootScope.page_name = next.params.page_name[0].toUpperCase() + next.params.page_name.slice(1);
-                $scope.page_url = '/views/' + next.params.page_name + '.html';
-                $timeout(enableTransitions, 600);
+
+                if (next.params.page_name == 'about' || next.params.page_name == 'work' || next.params.page_name == 'contact') {
+                    $rootScope.page_name = next.params.page_name[0].toUpperCase() + next.params.page_name.slice(1);
+                    $scope.page_url = '/views/' + next.params.page_name + '.html';
+                    $timeout(enableTransitions, 600);
+                }
+                else {
+                    $location.url('/');
+                }
             }
             else {
+                next.params.page_name = '';
                 $rootScope.page_name = 'Home';
                 $animate.enabled(false);
                 angular.element(document.querySelector('.menu')).removeClass('enable');
