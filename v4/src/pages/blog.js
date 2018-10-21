75fe847215026aed76c1135b8dede6f1e4c31694
Michael Yuen <michaelclaytonyuen@gmail.com>
2018-10-21 16:14:24 -0400

    styling adjustments for ipad and ipad pro. fix class heirarchy on blog and contact pages.

--- v4/src/pages/blog.js
+++ v4/src/pages/blog.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 const BlogPage = () => (
 
-	<div className="MyContainer MyContainer__rectangle MyContainer__fixed">
-		<div>
-			<h1 className="center uppercase">blog</h1>
-			<p className="center">
-				Coming soon...ish
-			</p>
+	<div className="MyContainer__fixed">
+		<div className="MyContainer MyContainer__rectangle">
+			<div>
+				<h1 className="center uppercase">blog</h1>
+				<p className="center">
+					Coming soon...ish
+				</p>
+			</div>
 		</div>
 	</div>
 )
--- v4/src/pages/contact.js
+++ v4/src/pages/contact.js
@@ -2,39 +2,42 @@ import React from 'react'
 
 const ContactPage = () => (
 
-	<div className="MyContainer MyContainer__rectangle MyContainer__fixed">
-		<div>
+	<div className="MyContainer__fixed">
+		<div className="MyContainer MyContainer__rectangle">
+			<div>
 
-			<h1 className="center uppercase">contact</h1>
-			<p>
-				Thank you for visiting my website and I hope you enjoyed the experience. If you would like to contact me, the best method is to email me directly.
-			</p>
+				<h1 className="center uppercase">contact</h1>
+				<p>
+					Thank you for visiting my website and I hope you enjoyed the experience. If you would like to contact me, the best method is to email me directly.
+				</p>
 
-			<p>
-				For professional networking and more information about my job history and experience, visit my LinkedIn.
-			</p>
+				<p>
+					For professional networking and more information about my job history and experience, visit my LinkedIn.
+				</p>
 
-			<p>
-				If my website has sparked your curiosity, you can dig into the code at Github.
-			</p>
+				<p>
+					If my website has sparked your curiosity, you can dig into the code at Github.
+				</p>
 
-			<p>
-				I look forward to hearing from you!
-			</p>
+				<p>
+					I look forward to hearing from you!
+				</p>
 
-			<div className="row icons text-center">
+				<div className="row icons text-center">
 
-				<div className="col-xs-4">
-					<a className="fa fa-envelope fa-3x" href="mailto:michaelclaytonyuen@gmail.com">.</a>
+					<div className="col-xs-4">
+						<a className="fa fa-envelope fa-3x" href="mailto:michaelclaytonyuen@gmail.com">.</a>
+					</div>
+
+					<div className="col-xs-4">
+						<a className="fa fa-linkedin fa-3x" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/yuenmichael">.</a>
+					</div>
+
+					<div className="col-xs-4">
+						<a className="fa fa-github-alt fa-3x" target="_blank" rel="noopener noreferrer" href="https://www.github.com/michaelyuen">.</a>
+					</div>
 				</div>
 
-				<div className="col-xs-4">
-					<a className="fa fa-linkedin fa-3x" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/yuenmichael">.</a>
-				</div>
-
-				<div className="col-xs-4">
-					<a className="fa fa-github-alt fa-3x" target="_blank" rel="noopener noreferrer" href="https://www.github.com/michaelyuen">.</a>
-				</div>
 			</div>
 		</div>
 	</div>
