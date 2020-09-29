abaf1315fe571cecb4cc594258b40ae90e6aa51a
Michael Yuen <iam@mikey.baby>
2020-09-28 23:19:48 -0400

    Add blockquote component

--- v5/components/Note/index.tsx
+++ v5/components/Note/index.tsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import styled from "styled-components";
 import Date from "../Date";
+import Blockquote from "../Blockquote";
 import CodeBlock from "../CodeBlock";
 
 type Props = {
@@ -17,7 +18,9 @@ export default function Note({ className, data: { content, date } }: Props) {
   return (
     <NoteContainer className={className}>
       <Date dateString={date} />
-      <ReactMarkdown renderers={{ code: CodeBlock }}>{content}</ReactMarkdown>
+      <ReactMarkdown renderers={{ blockquote: Blockquote, code: CodeBlock }}>
+        {content}
+      </ReactMarkdown>
     </NoteContainer>
   );
 }
